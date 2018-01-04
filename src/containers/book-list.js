05633ff8791d57d7f9eddbcae599ceefc061182e
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-01-04 17:33:17 -0500

    booklist app

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
  class BookList extends Component{
 
@@ -8,7 +9,14 @@ renderlist(){
 
   return this.props.books.map((book) => {
 
-      return (<li className="list-group-item"> {book.title} </li>);
+      return (
+        <li
+        key={book.title}
+        onClick={() => this.props.selectBook(book)}
+        className="list-group-item">
+        {book.title}
+        </li>)
+        ;
   } );
 }
 
@@ -32,4 +40,9 @@ books: state.books
 
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+
+return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function(){
 return [
-{ title : 'b1'},
-{ title : 'b2'},
-{ title : 'b3'},
-{ title : 'b4'}
+{ title : 'b1', pages: 100},
+{ title : 'b2', pages: 200},
+{ title : 'b3', pages:300},
+{ title : 'b4', pages:400}
 ]
 
 }
